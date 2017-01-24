1eb5747d1f1cb072c9611682c173264cec64fa93
Óðinn Kristjánsson <odinnkr247@nemi.tskoli.is>
2017-01-24 14:23:16 +0000

    Liður3 og comment

--- forgit.py
+++ forgit.py
@@ -19,8 +19,22 @@ print("Þetta er summa talananna",utkoma1)
 print("Þetta er marfeldi talnanna",utkoma2)
 
 
-
-
-
-
-
+#Dæmi 3
+#Fær setningu og segir hversu margir lág og hástafir eru í henni og hversu oft lágstafir koma á eftir hástöfum
+lagstafur=0
+hastafur=0
+laghastafur=0
+texti=input("Sláðu in setningu")
+for staf in texti:
+    if staf.islower():
+        lagstafur=lagstafur+1
+    if staf.isupper():
+        hastafur=hastafur+1
+for i in range(len(texti)):
+    if texti[i].isupper():
+        hastafur=hastafur+1
+    if texti[i+1].islower():
+        laghastafur=laghastafur+1
+print("Það eru",lagstafur,"lágstafir")
+print("Það eru",hastafur,"hástafir")
+print("Það eru",laghastafur,"lágstafir sem koma á eftir hástaf")
