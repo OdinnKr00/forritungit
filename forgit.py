81d04966377099632884e11ae76ff00761b953fb
Óðinn Kristjánsson <odinnkr247@nemi.tskoli.is>
2017-01-24 13:57:35 +0000

    Fyrsta comment

--- forgit.py
+++ forgit.py
@@ -3,16 +3,24 @@
 
 
 #Dæmi 2
+#Fær fornafn og eftirnafn og prentar út halló með fullu nafni
 fornafn=input("Sláðu inn fyrsta nafnið þitt")
 eftirnafn=input("Sláðu inn eftirnafnið þitt")
 print("Halló",fornafn,eftirnafn)
 
 
 #Dæmi 1
+
 tala1=int(input("Sláðu inn tölu"))
 tala2=int(input("Sláðu inn aðra tölu"))
 utkoma1=tala1+tala2
 utkoma2=tala1*tala2
 print("Þetta er summa talananna",utkoma1)
 print("Þetta er marfeldi talnanna",utkoma2)
+
+
+
+
+
+
 
