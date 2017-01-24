06ac8ee8c873fa94e54dbbf8c2df044e11c49ad4
Óðinn Kristjánsson <odinnkr247@nemi.tskoli.is>
2017-01-24 13:45:51 +0000

    Liður 2(1)

--- forgit.py
+++ forgit.py
@@ -2,7 +2,17 @@
 #24.1.2017
 
 
-#Dæmi 1
+#Dæmi 2
 fornafn=input("Sláðu inn fyrsta nafnið þitt")
 eftirnafn=input("Sláðu inn eftirnafnið þitt")
 print("Halló",fornafn,eftirnafn)
+
+
+#Dæmi 1
+tala1=int(input("Sláðu inn tölu"))
+tala2=int(input("Sláðu inn aðra tölu"))
+utkoma1=tala1+tala2
+utkoma2=tala1*tala2
+print("Þetta er summa talananna",utkoma1)
+print("Þetta er marfeldi talnanna",utkoma2)
+
